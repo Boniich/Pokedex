1dfdd6b243f8afc49cfe164f4cd97e73962560f1
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-12-17 14:03:47 -0300

    modeling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Nav from "./components/Nav";
 import PokeAjax from "./components/PokeAjax";
 import "./App.css"
+import Hero from "./components/hero/Hero";
 
 
 
@@ -11,7 +12,24 @@ function App() {
         <Nav />
       </header>
       <section className="conteiner">
-        <PokeAjax />
+        <div className="search-bar">Search Bar</div>
+        <div className="colum">
+          <div className="region">
+            <h1 className="name-title">Region</h1>
+          </div>
+          <div className="select-type">
+            <h1 className="name-title">Type</h1>
+          </div>
+        </div>
+        <div className="content">
+          <div className="contador-orden">
+          <div className="contador">150 Pokemon</div>
+          <div className="orden">Orden</div>
+          </div>
+
+          <div className="grid-fluid"><PokeAjax /></div>
+          </div>
+        
       </section>
     </div>
   );
