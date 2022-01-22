feb1a13a061e375ffff7987e2f3cab70ce117779
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-01-22 11:22:21 -0300

    added a hooks that handle the state of pokemons

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
 import Nav from "./components/Nav";
-import PokeAjax from "./components/PokeAjax";
 import "./App.css"
-import Hero from "./components/hero/Hero";
+import SearchBar from "./components/searchBar/searchBar";
+import ContainsRegionAndType from "./components/columRegionType/containsRegionAndType/ContainsRegionAndType";
+import Content from "./components/content/Content";
+import { useState } from "react";
 
-
+const initialPokemon = "limit=150&offset=0";
 
 function App() {
+
+  const [pokemon, setPokemon] = useState(initialPokemon);
+  const handleRegion = () => {}
+
   return (
     <div className="App">
       <header className="App-header">
         <Nav />
       </header>
       <section className="conteiner">
-        <div className="search-bar">Search Bar</div>
-        <div className="colum">
-          <div className="region">
-            <h1 className="name-title">Region</h1>
-          </div>
-          <div className="select-type">
-            <h1 className="name-title">Type</h1>
-          </div>
-        </div>
-        <div className="content">
-          <div className="contador-orden">
-          <div className="contador">150 Pokemon</div>
-          <div className="orden">Orden</div>
-          </div>
-
-          <div className="grid-fluid"><PokeAjax /></div>
-          </div>
-        
+        <SearchBar />
+        <ContainsRegionAndType />
+        <Content pokemon={pokemon}/>
       </section>
     </div>
   );
