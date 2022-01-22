e8ff3c0c2781cacfec49a846c8a5d469806480d2
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-01-22 11:23:46 -0300

    recive a new prop

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -2,14 +2,14 @@ import React from "react";
 import PokeAjax from "../PokeAjax";
 import CounterAndOrder from "./counterAndOrder/CounterAndOrder";
 
-function Content(){
+function Content({pokemon}){
 
     return(
 
         <div className="content">
         <CounterAndOrder />
         <div className="grid-fluid">
-        <PokeAjax />
+        <PokeAjax pokemon={pokemon}/>
         </div>
         </div>
 
