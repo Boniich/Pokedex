6ac9bd3e309415345bb3406d358051602e1c139a
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-01-22 15:32:56 -0300

    basic consts defined

---
+++ src/consts/index.js
@@ -0,0 +1,7 @@
+export const KANTO = "limit=150&offset=0";
+export const JOHTO = "limit=100&offset=151";
+export const HOENN = "";
+export const SINNOH = "";
+export const TESELIA = "";
+export const KALOS = "";
+export const ALOLA = "";
